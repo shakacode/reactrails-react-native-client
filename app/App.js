a02745c1f76d6943e88ee4b0dbec0901f35f6599
Alexey Karasev <karasev.alexey@gmail.com>
2016-07-07 21:40:57 +0500

    initial commit

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './AppStyle';
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Welcome to React Native!
+    </Text>
+  </View>
+);
+
+export default App;
